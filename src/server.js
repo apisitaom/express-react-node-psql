890e2d9f396951b6dc8e54da22e8274aa8dd96e7
apisitprompha <apisitprompha@gmail.com>
2019-08-19 08:12:30 +0700

    add server for get start project

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const cors = require('express')
+const cors = require('cors')
 
 //ROUTES
 const routes = require('./routes/index')
@@ -9,18 +9,17 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.use(cors())
-app.use(express.json)
-
-app.get('/',(req,res)=>{
-    res.send('PROJECT')
-})
-
+// app.use(express.json)
 app.use((req,res,next)=>{
     console.log('Time: %d', Date.now())
     next()
 })
 
+app.get('/',(req,res)=>{
+    res.send('PROJECT')
 
-app.listen(port,()=>{
+})
+
+app.listen(port,function(){
     console.log(`server run on port ${port}`)
 })
