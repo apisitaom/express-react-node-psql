3dce73e6e1be6be0eee7f4488c4d2dcbcdafbd3c
apisitprompha <apisitprompha@gmail.com>
2019-08-19 08:35:57 +0700

    add three file most edit for server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-
+const reqip = require('request-ip');
 //ROUTES
-const routes = require('./routes/index')
+const seller = require('./routes/index')
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -11,13 +11,32 @@ const port = process.env.PORT || 4000
 app.use(cors())
 // app.use(express.json)
 app.use((req,res,next)=>{
-    console.log('Time: %d', Date.now())
+    const clientIp = reqip.getClientIp(req); 
+    let currentDatetime = new Date()
+    let formattedDate = currentDatetime.getFullYear() + "-" + (currentDatetime.getMonth() + 1) + "-" + currentDatetime.getDate() + " " + currentDatetime.getHours() + ":" + currentDatetime.getMinutes() + ":" + currentDatetime.getSeconds() 
+    console.log(`Date = `+formattedDate+`, IP IPV4 = `+clientIp)
     next()
 })
 
-app.get('/',(req,res)=>{
-    res.send('PROJECT')
+// app.use('/seller',seller);    //Because it not exports moduls
 
+app.get('/',(req,res)=>{
+    res.send('!@%^#!@&#%!@&#%===PROJECT===!*@&(#!@^&#*!@$^!@&*')
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    if(err.status === 500 || err.status === 404){
+        err.message = `server error`
+    }
+    next()
+})
+
+app.use((err,req,res,next)=>{
+    res.status(500).send({
+        code: 500,
+        msg: err.message
+    })
 })
 
 app.listen(port,function(){
